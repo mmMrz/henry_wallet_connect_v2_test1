772b03af82d32e8d9dd62b62060cde6bc2a4412b
mmMrz <313371307@qq.com>
2023-10-01 22:48:51 +0800

    update chain config to entity

---
+++ lib/bean/chain_config_bean.dart
@@ -0,0 +1,14 @@
+class ChainConfigBean {
+  String? name;
+  String? caip2Id;
+  String? rpcUrl;
+  String? symbol;
+  String? blockExplorerUrl;
+  ChainConfigBean({
+    this.name,
+    this.caip2Id,
+    this.rpcUrl,
+    this.symbol,
+    this.blockExplorerUrl,
+  });
+}
